c9845cf05ec83ec8a8918367802c431a87b3a370
Arthur Verkaik <aquarthur@yahoo.com>
2015-10-20 20:35:47 +0100

    Added all the tables from the schema using SQLAlchemy's ORM
    Also added the necessary import statements and commented the
    new additions.

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
 # Configurations
 USERNAME = "admin" # (temporary check until database is working)
 PASSWORD = "password"
@@ -6,6 +9,9 @@ PASSWORD = "password"
 # Create app object
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///slipDB'
+app.config['SQLALCHEMY_NATIVE_UNICODE'] = True
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def home(name=None):
@@ -18,6 +24,27 @@ def home(name=None):
 def test():
     return 'The web site you are trying to reach is undergoing construction by a team of highly trained monkies. Please visit another time!'
 
+# Database Schema
+# Users Table
+class Users(db.Model):
+    userid = db.Column(UUID, primary_key=True)
+    username = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    firstname = db.Column(db.String(30), nullable=False)
+    lastname = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+# Raspberries Table
+class Raspberries(db.Model):
+    raspberryid = db.Column(UUID, primary_key=True)
+    userid = db.Column(UUID, db.ForeignKey('user.userid'))
+    
+# Events Table
+class Events(db.Model):
+    raspberryid = db.Column(UUID, db.ForeignKey('raspberries.raspberryid'), primary_key=True)
+    eventid = db.Column(db.String(10), nullable=False, primary_key=True)
+    eventtime = db.Column(db.DateTime, default=datetime.utcnow(), primary_key=True)
+    description = db.Column(db.Text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
