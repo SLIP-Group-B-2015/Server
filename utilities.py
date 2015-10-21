c54a0cf1c3ae577a90693c571fe16b1113928d47
Arthur Verkaik <aquarthur@yahoo.com>
2015-10-21 12:49:04 +0100

    Started Documentation. User now returns UUID in string format.

--- utilities.py
+++ utilities.py
@@ -15,14 +15,18 @@ def _commitChange(newRow):
     db.session.add(newRow)
     db.session.commit()
 
+# Return UUID
 def addUser(username, email, firstName, lastName, password):
     newUser = Users(userid=str(uuid.uuid4()), username=username, email=email, firstname=firstName, lastname=lastName, password=pbkdf2_sha256.encrypt(password, rounds=2000, salt_size=16))
     _commitChange(newUser)
+    newUserID = str(Users.query.filter_by(username=username).with_entities(Users.userid).all())
+    return newUserID[3:-4]
 
+# Return Boolean
 def addRaspberry(raspberryID, userID):
     newRaspberry = Raspberries(raspberryid=raspberryID, userid=userID)
     _commitChange(newRaspberry)
-
+# Return
 def addEvent(raspberryID, eventID, description):
     newEvent = Events(raspberryid=raspberryID, eventid=eventID, description=description)
     _commitChange(newEvent)
