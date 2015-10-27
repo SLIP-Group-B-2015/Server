d1b6ddb78c4b33b94846643741596df0ceddf1b3
Jamie McCracken <jimcc007@hotmail.co.uk>
2015-10-27 18:58:06 +0000

    still debugging

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ PASSWORD = "password"
 
 # Create app object
 app = Flask(__name__)
+app.debug = True
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///slipDB'
 app.config['SQLALCHEMY_NATIVE_UNICODE'] = True
