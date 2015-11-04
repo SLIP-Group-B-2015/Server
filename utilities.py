38a01707607a51ee90b079e190c6ce6d632b2326
Arthur Verkaik <aquarthur@yahoo.com>
2015-11-04 21:31:11 +0000

    Renamed unpackJSON and generateJSON and split them into several functions.
    This was to make the function more flexible and easier to read.
    postJSON (renamed from unpackJSON) was split into piPosts,
    phonePosts and phoneGets.
    
    piPosts deals with POST requests from the raspberry pi and returns
    a boolean whether an event was added to the table or not.
    
    phonePosts deals with POST requests from phones. If this is a
    REGISTER request, it returns the new userid if successful and False
    if not. For ADDPI, it returns the appropriate boolean based on the
    success of the db modification.
    
    getJSON (renamed from generateJSON) now builds an appropriate JSON
    object depending on the given eventType. It uses functions phoneGets,
    getEvents, getPies, and checkLogin.
    
    phoneGets deals with phones' GET requests, returning a list of
    JSON-formatted objects containing event information. It uses getEvents
    to get all the appropriate events (depending on the event type and
    option).
    
    getPies returns a list of raspberry ids connected to a
    specified user.checkLogin just returns simple json, which depends on whether
    the login details are correct or not.
    
    Also added functions connectUserToRaspberry (self-explanatory), and
    checkUser, which checks if a username/email already exists in the
    database and returns a boolean.

--- utilities.py
+++ utilities.py
@@ -1,10 +1,15 @@
-# Arthur Verkaik
-# s1247563
-# Utility functions for the server
-
+'''
+Arthur Verkaik
+s1247563
+Utility functions for the server
 # TODO
-# - Document each functions
-# - Create new function for retrieving events/users using the raspberryID.
+  - Documentation for each function
+  - db.rollbacks() where necessary
+  - create testing functions (generateUsers, for example).
+  - abstract away large queries.
+  - add "INVALID EVENT TYPE" errors everywhere.
+'''
+
 
 import json
 import uuid
@@ -15,12 +20,24 @@ def _commitChange(newRow):
     db.session.add(newRow)
     db.session.commit()
 
-# Return UUID
+def checkUser(username, email):
+    if (len(db.session.query(Users).filter(Users.email=email).all()) == 0 && \
+        len(db.session.query(Users).filter(Users.username==username).all()) == 0):
+        return True
+    else:
+        return False
+
+# Return Boolean
 def addUser(username, email, firstName, lastName, password):
-    newUser = Users(userid=str(uuid.uuid4()), username=username, email=email, firstname=firstName, lastname=lastName, password=pbkdf2_sha256.encrypt(password, rounds=2000, salt_size=16))
-    _commitChange(newUser)
-    newUserID = str(Users.query.filter_by(username=username).with_entities(Users.userid).all())
-    return newUserID[3:-4]
+    if (checkUser(username, email)):
+        newUser = Users(userid=str(uuid.uuid4()), username=username, email=email, \
+                        firstname=firstName, lastname=lastName, \
+                        password=pbkdf2_sha256.encrypt(password, rounds=2000, salt_size=16))
+        _commitChange(newUser)
+        newUserID = Users.query.filter_by(username=username).with_entities(Users.userid).first()
+        return newUserID.userid
+    else:
+        return False
 
 # Return Boolean
 def addRaspberry(raspberryID):
@@ -32,44 +49,149 @@ def addRaspberry(raspberryID):
     else:
         return False
 
-# Return void
+# Return boolean
+def connectUserToRaspberry(userID, raspberryID):
+    raspberryRow = Raspberries.query.filter_by(raspberryid=raspberryID).first()
+    if raspberryRow.userid == None:
+        raspberryRow.userid = userID
+        db.session.commit()
+        return True
+    else:
+        return False
+
+# Return Boolean
 def addEvent(raspberryID, eventType, eventTime, note, name):
+    if (len(db.session.query(Events).filter(Events.raspberryid==raspberryID).\
+            filter(Events.eventtype==eventType).filter(Events.eventtime==eventTime).\
+            all()) > 0):
+        return False
     newEvent = Events(raspberryid=raspberryID, eventtype=eventType, eventtime=eventTime, note=note, name=name)
     _commitChange(newEvent)
+    return True
 
-
-def unpackJSON(inputJSON):
-    # event types - ID_SCAN, KNOCK, MAIL, OPEN, CLOSE
-    data = json.loads(inputJSON)
-    eventType = str(json.loads(inputJSON)[u'event'])
-    eventTime = str(json.loads(inputJSON)[u'time'])
-    raspberryID = str(json.loads(inputJSON)[u'raspberry'])
+# Deals with pi POST requests. Returns a boolean (True = successfully added)
+def piPosts(data, eventType):
+    # possible event types: OPEN, CLOSE, KNOCK, MAIL, ID_SCAN
+    eventTime = str(data[u'time'])
+    raspberryID = str(data[u'raspberry'])
     note = None
     if data.get('note'):
-        note = str(json.loads(inputJSON)[u'note'])
-    name = None
+        note = str(data[u'note'])
+    fullName = None
 
     if eventType == 'ID_SCAN':
         userID = str(json.loads(inputJSON)[u'user'])
         bothNames = Users.query.filter_by(userid=userID).with_entities(Users.firstname,Users.lastname).all()
-        firstName = bothNames[0][0]
-        lastName = bothNames[0][1]
-        name = firstName + ' ' + lastName
+        fullName = bothNames[0][0] + ' ' + bothNames[0][1]
 
-    if (len(db.session.query(Events).filter(Events.raspberryid==raspberryID).filter(Events.eventtype==eventType).filter(Events.eventtime==eventTime).all()) >  0):
-        return "Event already exists!"
-    
-    addEvent(raspberryID, eventType, eventTime, note, name)
-    return "Event successfully added!"
+    return addEvent(raspberryID, eventType, eventTime, note, fullName)
+
+# Deals with phone POST requests. Returns the UUID if successful. Returns False if not.
+def phonePosts(data, eventType):
+    # json format: {event:REGISTER, username:abc, email:abc@mail.com, firstName:ab, lastName:c, password:abcd}
+    # event types: REGISTER
+
+    if eventType == 'REGISTER':
+        username = str(data[u'username'])
+        password = str(data[u'password'])
+        email = str(data[u'email'])
+        firstName = str(data[u'firstName'])
+        lastName = str(data[u'lastName'])
+        return addUser(username, email, firstName, lastName, password)
+
+    elif eventType == 'ADDPI':
+        raspberryID = str(data[u'raspberryid'])
+        userID = str(data[u'userid'])
+        return connectUserToRaspberry(raspberryID, userID)
+
+def postJSON(inputJSON):
+    # phone event types - REGISTER, ADDPI
+    # pi event types - ID_SCAN, KNOCK, MAIL, OPEN, CLOSE
+    piEvents = ['ID_SCAN', 'KNOCK', 'MAIL', 'OPEN', 'CLOSE']
+    phonePostEvents = ['REGISTER']
+    data = json.loads(inputJSON)
+    eventType = str(data[u'event'])
+
+    if eventType in piEvents:
+        return piPosts(data, eventType)
+    elif eventType in phonePostEvents:
+        return phonePosts(data, eventType)
+
  ##  {"event":"ID_SCAN","time":"Sat Oct 24 19:13:00 BST 2015","raspberry":"b673ab6f-182e-4c95-9715-ba8587fa33ca","user":"b673ab6f-182e-4c95-9715-ba8587fa33ca"}
-def generateJSON(inputJSON):
-    userID = str(json.loads(inputJSON)[u'userid'])
-    unsentEvents = db.session.query(Events).filter(Users.userid==userID).filter(Users.userid==Raspberries.userid).filter(Raspberries.raspberryid==Events.raspberryid).filter(Events.sent==False).all()
-    eventList = []
-    for i in unsentEvents:
-        event = {"eventType": i.eventtype, "eventtime": str(i.eventtime), "note": i.note, "name": i.name}
-        eventList.append(event)
+
+def getEvents(eventType, userID, option):
+
+    eventsBaseQuery = db.session.query(Events).filter(Users.userid==userID).\
+                 filter(Users.userid==Raspberries.userid).\
+                 filter(Raspberries.raspberryid==Events.raspberryid)
+    if eventType == 'UNSENT':
+        if option == 1:
+            events = eventsBaseQuery.filter(Events.sent==False).all()
+        elif option == 0:
+            events = eventsBaseQuery.filter(Events.sent==False).\
+                     filter(Events.eventtype!='OPEN').\
+                     filter(Events.eventtype!='CLOSE').all()
+    elif eventType == 'ALL':
+        if option == 1:
+            events = eventsBaseQuery.all()
+        elif option == 0:
+            events = eventsBaseQuery.filter(Events.eventtype!='OPEN').\
+                     filter(Events.eventtype!='CLOSE').all()
+    elif eventType in ['GETKNOCK', 'GETMAIL','GETID_SCAN']:
+        events = eventsBaseQuery.filter(Events.eventtype==eventType[3:])
+
+    return events
+
+def phoneGets(eventType, userID, option):
+    events = getEvents(eventType, userID, option)
+    jsonEventList = []
+
+    for i in events:
+        jsonEvent = {"raspberryID":i.raspberryid, "eventType":i.eventtype, "eventTime":str(i.eventtime), "note": i.note, "name": i.name}
+        jsonEventList.append(jsonEvent)
         i.sent = True
         db.session.commit()
 
-    return json.dumps({'newEvents': eventList})
+    return jsonEventList
+
+def getPies(userID):
+    raspberryIDList = []
+    connectedRaspberries = Raspberries.query.filter(Raspberries.userid==userID).all()
+    for i in connectedRaspberries:
+        raspberry = {"raspberryID":i.raspberryid}
+    return connectedRaspberries
+
+def checkLogin(userID, password):
+    hashedPassword =  Users.query.filter_by(userid=userID).with_entities(Users.password).all()
+    return pbkdf2_sha256.verify(password, hashedPassword)
+
+def getJSON(inputJSON):
+    # Phone events (other than the list below): LOGIN, GETPI
+    phoneGetEvents = ['ALL', 'UNSENT', 'GETKNOCK', 'GETMAIL', 'GETID_SCAN']
+    data = json.loads(inputJSON)
+    eventType = str(data[u'event'])
+
+    if eventType in phoneGetEvents:
+        userID = str(data[u'userid'])
+        option = str(data[u'option'])
+        jsonEventList = phoneGets(eventType, userID, option)
+        return json.dumps({'eventList': jsonEventList})
+
+    elif eventType == 'LOGIN':
+        # Deals with getting userID
+        username = str(data[u'username'])
+        userID = Users.query.filter(Users.username==username).with_entities(Users.userid).all()
+        if len(userID) == 0:
+            return json.dumps({'userid': 'DNE'})
+        else:
+            userID = userID[0].userid
+        password = str(data[u'password'])
+        if checkLogin(userID, password):
+            return json.dumps({'userid': userID})
+        else:
+            return json.dumps({'userid': 'DNE'})
+
+    elif eventType == 'GETPI':
+        userID = str(data[u'userid'])
+        raspberryList = getPies(userID)
+        return json.dumps({'raspberryList': raspberryList})
