eaf1981f3c3bc565e6916af0268cd6bcbfc8cd98
Arthur Verkaik <aquarthur@yahoo.com>
2015-11-24 03:57:22 +0000

    Refactored for consistency with SQLAlchemy/DB changes.
    Also added descriptions for each different functions and lightly
    refactored some of them. Further refactoring will take place tomorrow.

--- utilities.py
+++ utilities.py
@@ -4,7 +4,6 @@ s1247563
 Utility functions for the server
 # TODO
   - Documentation for each function
-  - db.rollbacks() where necessary
   - properly throw/catch errors
   - create testing functions (generateUsers, for example).
   - abstract away large queries.
@@ -14,11 +13,17 @@ Utility functions for the server
   - debug getEvents
 '''
 
-
-import json
-import uuid
+import json, uuid
 from passlib.hash import pbkdf2_sha256
-from server import db, Users, Raspberries, Events, Raspberry_names
+from server import db, Users, Connections, Events, Raspberries
+
+'''
+Function: _commitChange
+Input: newRow - this is a new entry in a table staged for commit.
+       noError - Boolean which is used to check whether change has been successfully
+                 committed or not.
+Output: Boolean which shows whether change has been successfully committed or not.
+'''
 
 def _commitChange(newRow, noError):
     try:
@@ -31,13 +36,28 @@ def _commitChange(newRow, noError):
 
     return noError
 
+'''
+Function: checkUser
+Input: username - string, and email - string (self-explanatory inputs).
+Output: Checks the given username and email against the database and returns
+a boolean. (True = user exists.)
+'''
+
 def checkUser(username, email):
     if len(Users.query.filter_by(email=email).all()) == 0 and len(Users.query.filter_by(username=username).all()) == 0:
         return True
     else:
         return False
 
-# Return Boolean
+
+'''
+Function: addUser
+Input: username, email, firstName, lastName, password (all strings, self-explanatory)
+Output: Adds a user with the given details to the database if no constraints are broken.
+If the addition has been succesful, the function returns the newly added user's uuid.
+If the operation failed, the function returns false.
+'''
+
 def addUser(username, email, firstName, lastName, password):
     if (checkUser(username, email)):
         newUser = Users(userid=str(uuid.uuid4()), username=username, email=email, \
@@ -49,30 +69,39 @@ def addUser(username, email, firstName, lastName, password):
     else:
         return False
 
-# Return Boolean
+'''
+Function: addRaspberry
+Input: raspberryID - a UUID in string format.
+Output: Adds a raspberry to the Raspberries table, if no constraints are broken.
+Returns the newly added pi's UUID if the operation was successful, otherwise,
+returns False.
+'''
 def addRaspberry(raspberryID):
-    print(raspberryID)
-    existingRaspberries = Raspberry_names.query.filter_by(raspberryid=raspberryID).all()
-    if (len(existingRaspberries) == 0):
-        newRaspberryName = Raspberry_names(raspberryid=raspberryID, raspberryname=None)
-        _commitChange(newRaspberryName, True)
-        return True
+    existingPis = Raspberries.query.filter_by(raspberryid=raspberryID).all()
+    if (len(existingPis) == 0):
+        newRaspberry = Raspberries(raspberryid=raspberryID)
+        _commitChange(newRaspberry, True)
+        newRaspberryID = Raspberries.query.filter_by(raspberryid=raspberryID).first()
+        return newRaspberryID.raspberryid
     else:
         return False
 
-# Return boolean
+
+'''
+Function: connectUserToRaspberry
+Input: userID, raspberryID (both UUIDs), raspberryName (string)
+Output: Adds a new entry in Connections which links the specified user and raspberry
+together. The name given as input is stored in the raspberryname column. The output
+is a boolean. If the data was successfully added, return True. Otherwise return False.
+'''
+
 def connectUserToRaspberry(userID, raspberryID, raspberryName):
-    #print(raspberryID)
-    raspberryNameRow = db.session.query(Raspberry_names).filter(Raspberry_names.raspberryid==raspberryID).first()
-    #print(raspberryNameRow)
 
-    if raspberryNameRow != None:
-        newRaspberryConnection = Raspberries(raspberryid=raspberryID, userid=userID)
-        db.session.add(newRaspberryConnection)
+    raspberry = Raspberries.query.filter(Raspberries.raspberryid==raspberryID).first()
 
-        if raspberryNameRow.raspberryname == None:
-            raspberryNameRow.raspberryname = raspberryName
-
+    if raspberry != None:
+        newConnection = Connections(raspberryid=raspberryID, userid=userID, raspberryname=raspberryName)
+        db.session.add(newConnection)
         try:
             db.session.commit()
             return True
@@ -82,17 +111,28 @@ def connectUserToRaspberry(userID, raspberryID, raspberryName):
     else:
         return False
 
-# Return Boolean
+'''
+Function: addEvent
+Input: raspberryID (UUID), eventType, eventTime, note, name (strings)
+Output: Adds an event if a replica doesn't already exist. If a replica exists, return
+False. Otherwise, return true.
+'''
 def addEvent(raspberryID, eventType, eventTime, note, name):
     if (len(db.session.query(Events).filter(Events.raspberryid==raspberryID).\
             filter(Events.eventtype==eventType).filter(Events.eventtime==eventTime).\
             all()) > 0):
-        return True
+        return False
 
     newEvent = Events(raspberryid=raspberryID, eventtype=eventType, eventtime=eventTime, note=note, name=name)
     return _commitChange(newEvent, True)
 
 # Deals with pi POST requests. Returns a boolean (True = successfully added)
+'''
+Function: piPosts
+Input: data (json), eventType (string)
+Output: adds an event to the database with the given data. Returns the Output
+of addEvent (true if event added, false otherwise).
+'''
 def piPosts(data, eventType):
     # possible event types: OPEN, CLOSE, KNOCK, MAIL, ID_SCAN
     eventTime = str(data[u'time'])
@@ -110,6 +150,12 @@ def piPosts(data, eventType):
     return str(addEvent(raspberryID, eventType, eventTime, note, fullName))
 
 # Deals with phone POST requests. Returns the UUID if successful. Returns False if not.
+'''
+Function: phonePosts
+Input: data (json), eventType (string)
+Output: If the event is register, returns the output of addUser with the given
+data. If the event is adding a pi, returns the output of connectUserToRaspberry.
+'''
 def phonePosts(data, eventType):
     # json format: {event:REGISTER, username:abc, email:abc@mail.com, firstName:ab, lastName:c, password:abcd}
     # event types: REGISTER
@@ -130,6 +176,14 @@ def phonePosts(data, eventType):
         added = str(connectUserToRaspberry(userID, raspberryID, raspberryName))
         return json.dumps({'added': added})
 
+'''
+The function which deals with any kind of post (from phone or pi)
+Function: postJSON
+Input: inputJSON (json object)
+Output: the output of the appropriate post function (if the request is from
+a phone, use phonePosts, if from a pi, use piPosts).
+'''
+
 def postJSON(inputJSON):
     # phone event types - REGISTER, ADDPI
     # pi event types - ID_SCAN, KNOCK, MAIL, OPEN, CLOSE
@@ -143,13 +197,17 @@ def postJSON(inputJSON):
     elif eventType in phonePostEvents:
         return phonePosts(data, eventType)
 
- ##  {"event":"ID_SCAN","time":"Sat Oct 24 19:13:00 BST 2015","raspberry":"b673ab6f-182e-4c95-9715-ba8587fa33ca","user":"b673ab6f-182e-4c95-9715-ba8587fa33ca"}
-
+'''
+Function: getEvents
+Input: eventType (string), userID (UUID), option (binary number)
+Output: returns a list of events. The query which gets these events is determined
+by the inputs. Supports ALL, UNSENT, and GETKNOCK/MAIL/ID_SCAN.
+'''
 def getEvents(eventType, userID, option):
     events = []
     eventsBaseQuery = db.session.query(Events).filter(Users.userid==userID).\
-                 filter(Users.userid==Raspberries.userid).\
-                 filter(Raspberries.raspberryid==Events.raspberryid)
+                 filter(Users.userid==Connections.userid).\
+                 filter(Connections.raspberryid==Events.raspberryid)
     if eventType == 'UNSENT':
         if option == 1:
             events = eventsBaseQuery.filter(Events.sent==False).all()
@@ -168,6 +226,13 @@ def getEvents(eventType, userID, option):
 
     return events
 
+'''
+Function: phoneGets
+Input: events (list of events)
+Output: a json containing one object. This object is a list of all the events in
+the input, and will be sent to the phone.
+'''
+
 def phoneGets(events):
     jsonEventList = []
 
@@ -182,20 +247,39 @@ def phoneGets(events):
 
     return jsonEventList
 
+'''
+Function: getPies
+Input: userID (UUID)
+Output: Returns the list of raspberries connected to the specified user.
+'''
+
 def getPies(userID):
     raspberryList = []
-    connectedRaspberries = Raspberries.query.filter(Raspberries.userid==userID).all()
-    for i in connectedRaspberries:
-        raspberryName = Raspberry_names.query.filter(Raspberry_names.raspberryid==i.raspberryid).first()
-        raspberryName = raspberryName.raspberryname
-        raspberry = {"raspberryID":i.raspberryid, "raspberryName":raspberryName}
+    connectedPis = Connections.query.filter(Connections.userid==userID).all()
+    for i in connectedPis:
+        raspberryRow = Raspberries.query.filter(Raspberries.raspberryid==i.raspberryid).first()
+        raspberry = {"raspberryID":i.raspberryid, "raspberryName":i.raspberryName}
         raspberryList.append(raspberry)
     return raspberryList
 
+'''
+Function: checkLogin
+Input: userID (string), password(string)
+Output: boolean. if the given details match the stored details, return True. Otherwise,
+return False.
+'''
 def checkLogin(userID, password):
     hashedPassword =  Users.query.filter_by(userid=userID).with_entities(Users.password).first()
     return pbkdf2_sha256.verify(password, hashedPassword.password)
 
+'''
+Function: getJSON
+Input: inputJSON (json object)
+Output: This is the function that deals with GET requests. Depending on the event type,
+we know which device has sent the request. We then use the appropriate functions to
+determine the output of the function, a json with all the information the phone/pi
+needs.
+'''
 def getJSON(inputJSON):
     # Phone events (other than the list below): LOGIN, GETPI
     phoneGetEvents = ['ALL', 'UNSENT', 'GETKNOCK', 'GETMAIL', 'GETID_SCAN']
